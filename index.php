639abcaffa8f6ce67d12e77eb510dd054257ab8a
Angela Amacna <6031@htl.rennweg.at>
2019-01-28 13:01:57 +0100

    [TASK] Setup doctrine configuration

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
  * Date: 28.01.2019
  * Time: 11:50
  */
+require_once "config.php";
+require "vendor/autoload.php";
 
-echo "hello World";
+$config = new \Doctrine\DBAL\Configuration();
+
+$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
