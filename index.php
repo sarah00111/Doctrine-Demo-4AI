d2b1b8efde99cd64aba8ba5242e2fe69b716cedc
Maximilian Groß <maximilian@elektro-gross.at>
2019-02-10 17:26:51 +0100

    [TASK] QueryBuilder/DBAL

--- index.php
+++ index.php
@@ -11,3 +11,20 @@ require "vendor/autoload.php";
 $config = new \Doctrine\DBAL\Configuration();
 
 $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+/*Schema(Database) erstellen*/
+/*Tables erstellen*/
+/*Daten einfügen*/
+
+$queryBuilder = $conn->createQueryBuilder();
+$inputAutor = 1;
+$qry = $queryBuilder
+    ->select('pk_autor', 'fk_blog')
+    ->from('autor')
+    ->where('pk_autor = ?')
+    ->setParameter(0, $inputAutor);
+$sth = $qry->execute();
+$result = $sth->fetchAll();
+foreach ($result as $row) {
+    echo "Autor: ".$row['pk_autor'] . "|" . " Blog:" . $row['fk_blog'];
+}
